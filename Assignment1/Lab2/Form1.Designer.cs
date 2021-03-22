7f585cf5ddaa1c98c81f4dfd93b94c43c4a6d5d0
bianca <bianca.paula2505@gmail.com>
2021-03-22 17:54:55 +0200

    First Assignment

---
+++ Assignment1/Lab2/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgCountries = new System.Windows.Forms.DataGridView();
+            this.dgClients = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_SaveChanges = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgCountries)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgClients)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgCountries
+            // 
+            this.dgCountries.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgCountries.Location = new System.Drawing.Point(25, 119);
+            this.dgCountries.Name = "dgCountries";
+            this.dgCountries.RowHeadersWidth = 51;
+            this.dgCountries.RowTemplate.Height = 24;
+            this.dgCountries.Size = new System.Drawing.Size(471, 223);
+            this.dgCountries.TabIndex = 0;
+            // 
+            // dgClients
+            // 
+            this.dgClients.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgClients.Location = new System.Drawing.Point(537, 119);
+            this.dgClients.Name = "dgClients";
+            this.dgClients.RowHeadersWidth = 51;
+            this.dgClients.RowTemplate.Height = 24;
+            this.dgClients.Size = new System.Drawing.Size(738, 489);
+            this.dgClients.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(20, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Country";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(532, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Client";
+            // 
+            // btn_SaveChanges
+            // 
+            this.btn_SaveChanges.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btn_SaveChanges.Location = new System.Drawing.Point(25, 376);
+            this.btn_SaveChanges.Name = "btn_SaveChanges";
+            this.btn_SaveChanges.Size = new System.Drawing.Size(471, 47);
+            this.btn_SaveChanges.TabIndex = 4;
+            this.btn_SaveChanges.Text = "Save changes";
+            this.btn_SaveChanges.UseVisualStyleBackColor = false;
+            this.btn_SaveChanges.Click += new System.EventHandler(this.btn_SaveChanges_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Monotype Corsiva", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(472, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(286, 37);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Art Gallery Management";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.buttonExit.Location = new System.Drawing.Point(25, 453);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(471, 47);
+            this.buttonExit.TabIndex = 6;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = false;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.ClientSize = new System.Drawing.Size(1322, 631);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_SaveChanges);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgClients);
+            this.Controls.Add(this.dgCountries);
+            this.Name = "Form1";
+            this.Text = "Assignment2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgCountries)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgClients)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgCountries;
+        private System.Windows.Forms.DataGridView dgClients;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_SaveChanges;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonExit;
+    }
+}
+
---
+++ Assignment1/Lab2/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace Lab2
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        DataSet ds;
+        SqlDataAdapter daCountries, daClients;
+        SqlCommandBuilder cbClients;
+        BindingSource bsCountries, bsClients;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(@"Data Source=DESKTOP-MGSMSKT\SQLEXPRESS; Initial Catalog=ArtGalleryDB; "
++ "Integrated Security=SSPI;");
+            ds = new DataSet();
+            daCountries = new SqlDataAdapter("SELECT * FROM Country", conn);
+            daClients = new SqlDataAdapter("SELECT * FROM Client", conn);
+            cbClients = new SqlCommandBuilder(daClients);
+            daCountries.Fill(ds, "Country");
+            daClients.Fill(ds, "Client");
+
+            DataRelation dr=new DataRelation("FK_Client_Countries",ds.Tables["Country"].Columns["CountryID"],
+                ds.Tables["Client"].Columns["CountryID"]);
+
+            ds.Relations.Add(dr);
+
+            bsCountries = new BindingSource();
+            bsCountries.DataSource = ds;
+            bsCountries.DataMember = "Country";
+
+            bsClients= new BindingSource();
+            bsClients.DataSource = bsCountries;
+            bsClients.DataMember = "FK_Client_Countries";
+
+            dgCountries.DataSource = bsCountries;
+            dgClients.DataSource = bsClients;
+        }
+
+        private void btn_SaveChanges_Click(object sender, EventArgs e)
+        {
+            daClients.Update(ds, "Client");
+        }
+
+
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+    }
+}
